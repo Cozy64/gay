1a4f16bf044bbeec27458e04e72222889fa18a25
Cozy64 <cozy.fun64@gmail.com>
2021-12-19 12:45:28 +0800

    added php.index

---
+++ gays/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gay</title>
+  </head>
+  <body>
+		<?php
+			echo "poo<br>poo<br>";
+			echo strlen("bruh");
+			?>
+		<style> h1{font-size:200px}
+		</style>
+		<h1>Gay</h1>
+		<script>
+			alert("No Gays Allowed.");
+			console.log("what the dog doin");
+		</script>
+	</body>
+</html>
+
